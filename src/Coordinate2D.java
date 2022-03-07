7fa3610e34cb6ea68017d432f55116b891706f7d
Dan Fielden <dan@danfielden.com>
2022-03-07 17:03:34 +0000

    2d and 3d coordinate classes

---
+++ src/Coordinate2D.java
@@ -0,0 +1,45 @@
+
+public final class Coordinate2D {
+    private final int x;
+    private final int y;
+
+    public Coordinate2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /** Returns the euler distance. */
+    private double magnitude() {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+
+    public Coordinate2D add(Coordinate2D c) {
+        return new Coordinate2D(this.x + c.x, this.y + c.y);
+    }
+
+    public static Coordinate2D add(Coordinate2D a, Coordinate2D b) {
+        return new Coordinate2D(a.x + b.x, a.y + b.y);
+    }
+
+    @Override
+    public String toString() {
+        return "[x=" + x + ", y=" + y + "]";
+    }
+
+    @Override
+    public boolean equals(Object compared) {
+        if (!(compared instanceof Coordinate2D)) {
+            return false;
+        }
+        if (this == compared) {
+            return true;
+        }
+        Coordinate2D c = (Coordinate2D) compared;
+        return c.x == this.x && c.y == this.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(x) * 31 + Integer.hashCode(y);
+    }
+}
---
+++ src/Coordinate3D.java
@@ -0,0 +1,46 @@
+public final class Coordinate3D {
+    private final int x;
+    private final int y;
+    private final int z;
+
+    public Coordinate3D(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /** Returns the euler distance. */
+    private double magnitude() {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+    }
+
+    public Coordinate3D add(Coordinate3D c) {
+        return new Coordinate3D(this.x + c.x, this.y + c.y, this.z + c.z);
+    }
+
+    public static Coordinate3D add(Coordinate3D a, Coordinate3D b) {
+        return new Coordinate3D(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    @Override
+    public String toString() {
+        return "[x=" + x + ", y=" + y + ", z=" + z + "]";
+    }
+
+    @Override
+    public boolean equals(Object compared) {
+        if (!(compared instanceof Coordinate3D)) {
+            return false;
+        }
+        if (this == compared) {
+            return true;
+        }
+        Coordinate3D c = (Coordinate3D) compared;
+        return c.x == this.x && c.y == this.y && c.z == this.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(x) * 31 + Integer.hashCode(y) + Integer.hashCode(z);
+    }
+}
