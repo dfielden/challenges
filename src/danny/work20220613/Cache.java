2d189146ecda6ed5add4b159bab01516bc4d29a7
Dan Fielden <dan@danfielden.com>
2022-06-13 13:21:32 +0100

    Cache challenge

--- src/danny/work20220613/Cache.java
+++ src/danny/work20220613/Cache.java
@@ -1,2 +1,37 @@
-package danny.work20220613;public class Cache {
+package danny.work20220613;
+import java.util.LinkedHashMap;
+
+/**
+ * A class for caching key-value pairs. If the key-value pair is not found in the cache, the supplied finder will
+ * attempt to return the appropriate value for the key parameter, and the returned key-value pair will be added to the
+ * cache if the value is not null.
+ */
+
+public final class Cache<K, V> {
+    private final LinkedHashMap<K, V> cache = new LinkedHashMap<>();
+    private final Fetcher<K, V> fetcher;
+    private final int cacheSize;
+
+    public Cache(Fetcher<K, V> fetcher) {
+        this(fetcher, 100);
+    }
+
+    public Cache(Fetcher<K, V> fetcher, int cacheSize) {
+        this.fetcher = fetcher;
+        this.cacheSize = cacheSize;
+    }
+
+    public V lookup(K key) {
+        V value = cache.get(key);
+        if (value == null) {
+            value = fetcher.apply(key);
+            if (value != null) {
+                if (cache.size() == cacheSize) {
+                    cache.remove(cache.keySet().iterator().next());
+                }
+                cache.put(key, value);
+            }
+        }
+        return value;
+    }
 }
--- src/danny/work20220613/Fetcher.java
+++ src/danny/work20220613/Fetcher.java
@@ -1,2 +1,10 @@
-package danny.work20220613;public class Fetcher {
+package danny.work20220613;
+
+import java.util.function.Function;
+
+public class Fetcher<K, V> implements Function<K, V> {
+    @Override
+    public V apply(K k) {
+        return null;
+    }
 }
